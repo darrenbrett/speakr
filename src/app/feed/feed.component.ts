0f83273aa3cd66ab57f396acf81c545c92da0779
Darren King <darrenbrettking@hotmail.com>
2016-09-04 15:49:48 -0700

    Refactoring

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   moduleId: module.id,
@@ -10,44 +11,45 @@ export class FeedComponent implements OnInit {
 
   tweets = [
 
-  { body: 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.', author: 'Glen', avatar: 'glen.jpg', date: new Date(), retweets: ['Joe'], favorites: [] },
-  { body: 'Measuring programming progress by lines of code is like measuring aircraft building progress by weight', author: 'Joe', avatar: 'joe.jpg', date: new Date(), retweets: [], favorites: ['Mary'] },
-  { body: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', author: 'Mary', avatar: 'mary.jpg', date: new Date(), retweets: ['Glen'], favorites: ['Mary'] },
-  { body: 'People think that computer science is the art of geniuses but the actual reality is the opposite, just many people doing things that build on each other, like a wall of mini stones', author: 'Glen', avatar: 'glen.jpg', date: new Date(), retweets: ['Joe', 'Mary'], favorites: [] },
-  { body: 'You can’t have great software without a great team, and most software teams behave like dysfunctional families.', author: 'Joe', avatar: 'joe.jpg', date: new Date(), retweets: [], favorites: ['Mary', 'Glen'] },
+    { body: 'Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.', author: 'Glen', avatar: 'glen.jpg', date: new Date(), retweets: ['Joe'], favorites: [] },
+    { body: 'Measuring programming progress by lines of code is like measuring aircraft building progress by weight', author: 'Joe', avatar: 'joe.jpg', date: new Date(), retweets: [], favorites: ['Mary'] },
+    { body: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', author: 'Mary', avatar: 'mary.jpg', date: new Date(), retweets: ['Glen'], favorites: ['Mary'] },
+    { body: 'People think that computer science is the art of geniuses but the actual reality is the opposite, just many people doing things that build on each other, like a wall of mini stones', author: 'Glen', avatar: 'glen.jpg', date: new Date(), retweets: ['Joe', 'Mary'], favorites: [] },
+    { body: 'You can’t have great software without a great team, and most software teams behave like dysfunctional families.', author: 'Joe', avatar: 'joe.jpg', date: new Date(), retweets: [], favorites: ['Mary', 'Glen'] },
 
 
-  ];
+  ]
 
   tweetText = '';
 
-   constructor() { }
+  constructor(private userService : UserService) { }
 
   ngOnInit() {
   }
 
-  isUserInCollection(collection: string[], userId: string): boolean {
+  isUserInCollection(collection : string[], userId : string) : boolean {
 
-      return collection.indexOf(userId) !== -1;
+      return collection.indexOf(userId) != -1;
 
   }
 
   OnFavorite(tweet) {
-    if (!this.isUserInCollection(tweet.favorites, 'Glen')) {
-      tweet.favorites.push('Glen');
+    if (!this.isUserInCollection(tweet.favorites, this.userService.getCurrentUser())) {
+      tweet.favorites.push(this.userService.getCurrentUser());
     }
   }
 
   OnRetweet(tweet) {
-    if (!this.isUserInCollection(tweet.retweets, 'Glen')) {
-      tweet.retweets.push('Glen');
+    if (!this.isUserInCollection(tweet.retweets, this.userService.getCurrentUser())) {
+      tweet.retweets.push(this.userService.getCurrentUser());
     }
   }
 
   OnNewTweet() {
+    console.log(this.tweetText);
     this.tweets.unshift(
-      { body: this.tweetText, author: 'Glen', avatar: 'glen.jpg', date: new Date(), retweets: [], favorites: [] }
-    );
+	      { body: this.tweetText, author: this.userService.getCurrentUser(), avatar: 'glen.jpg', date: new Date(), retweets: [], favorites: [] }
+	    );
     this.tweetText = '';
   }
 
